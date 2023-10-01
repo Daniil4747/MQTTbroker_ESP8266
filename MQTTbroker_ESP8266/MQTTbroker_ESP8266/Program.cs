db925cabe912a8e3d60ec1618b4c15be98d01027
Daniil4747 <Berezinez2016@mail.ru>
2023-10-01 07:42:02 +0300

    Update
    Добавлены дополнительный компоненты, как для проверки работоспособности, та и дальнейшего внедрения в различные сервисы.

---
+++ MQTTbroker_ESP8266/MQTTbroker_ESP8266/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+namespace ConsoleBroker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ipE = Dns.GetHostAddresses(Dns.GetHostName())
+                   .First(a => a.AddressFamily == AddressFamily.InterNetwork).ToString();
+
+            //configure options
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionValidator(c =>
+                {
+                    Console.WriteLine($"{c.ClientId} connection validator for c.Endpoint: {c.Endpoint}");
+                    c.ReasonCode = MqttConnectReasonCode.Success;
+                })
+                .WithApplicationMessageInterceptor(context =>
+                {
+                    //Console.WriteLine("WithApplicationMessageInterceptor block merging data");
+                    //var newData = Encoding.UTF8.GetBytes(DateTime.Now.ToString("O"));
+                    //var oldData = context.ApplicationMessage.Payload;
+                    //var mergedData = newData.Concat(oldData).ToArray();
+                    //context.ApplicationMessage.Payload = mergedData;
+                })
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointBoundIPAddress(IPAddress.Parse(ipE))
+                .WithDefaultEndpointPort(1884);
+
+
+            //start server
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            mqttServer.StartAsync(optionsBuilder.Build()).Wait();
+
+            Console.WriteLine($"Broker is Running: Host: {mqttServer.Options.DefaultEndpointOptions.BoundInterNetworkAddress} Port: {mqttServer.Options.DefaultEndpointOptions.Port}");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+
+            //To keep the app running in container
+            //https://stackoverflow.com/questions/38549006/docker-container-exits-immediately-even-with-console-readline-in-a-net-core-c
+            Task.Run(() => Thread.Sleep(Timeout.Infinite)).Wait();
+
+            mqttServer.StopAsync().Wait();
+
+
+        }
+    }
+}
